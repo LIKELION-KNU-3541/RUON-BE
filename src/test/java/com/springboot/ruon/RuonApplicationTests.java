1d6337b4b6a87434bec144f5d53c0403ced2e805
tangerinem <redvelvet0625@gmail.com>
2026-08-04 22:12:30 +0900

    setting: setting up the project structure

---
+++ src/test/java/com/springboot/ruon/RuonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.ruon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
