14427bececede140c8451dab4c85406526fbe5d9
garden <113418319+Garden0728@users.noreply.github.com>
2026-08-12 21:04:22 +0900

    refactor: 제품 사용 상태(Enum)를 정리 (#38)
    기존 Usestatus(Enum)-> IN_USE(사용중) , FINISHED (사용완료), DICARDED(폐기)
    변경 -> IN_USE(사용중), DISCONTINUED(사용중단)

--- src/main/java/com/springboot/ruon/domain/product/entity/UsageStatus.java
+++ src/main/java/com/springboot/ruon/domain/product/entity/UsageStatus.java
@@ -4,7 +4,6 @@ package com.springboot.ruon.domain.product.entity;
  * 제품 사용 상태
  */
 public enum UsageStatus {
-    IN_USE,      // 사용중
-    FINISHED,    // 사용완료
-    DISCARDED    // 폐기
+    IN_USE,        // 사용중
+    DISCONTINUED   // 사용중단
 }
